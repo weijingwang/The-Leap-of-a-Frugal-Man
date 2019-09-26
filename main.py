62311eb94889fa781246ab0691bcc2f5bbd25a7b
Weijing Wang <weijingrocks@gmail.com>
2019-09-25 19:49:00 -0700

    day 5
    im basically done....

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ room3bg = pygame.image.load("./assets/room3.png")
 titlePic = pygame.image.load("./assets/title.png")
 playerR = pygame.image.load("./assets/player_right.png")
 playerL= pygame.image.load("./assets/player_left.png")
+player_chute = pygame.image.load("./assets/player_chute.png")
 player_fall= pygame.image.load("./assets/player_fall.png")
 
 frontground =pygame.image.load("./assets/frontground.png")
@@ -34,8 +35,12 @@ city = pygame.image.load("./assets/city.png")
 explosion1 = pygame.image.load("./assets/explosion1.png")
 explosion2 = pygame.image.load("./assets/explosion2.png")
 bird = pygame.image.load("./assets/bird.png")
+parachute_message = pygame.image.load("./assets/parachute_message.png")
 
 
+#win
+win1 =pygame.image.load("./assets/win1.png")
+win_text =pygame.image.load("./assets/win_text.png")
 
 #game cg
 death1 =pygame.image.load("./assets/death1.png")
@@ -313,16 +318,28 @@ def falling_game():
 
 	bird2Y = 400
 	bird2X=-80
-
+	pmX = -750
+	pmY = 150
+	pmSpeed = 2
+	pmAccel = 0
 
 	#fall objects
 	# building_o = falling_object(0,0,5)
+	#clock here--------------------------
+	clock = pygame.time.Clock()
+	counter, text = 0, '0'.rjust(3)
+	pygame.time.set_timer(pygame.USEREVENT, 1000)
+	#-----
 
 
 	while not done:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				quit()
+			if event.type == pygame.USEREVENT:
+				counter += 1
+				text = str(counter).rjust(3) if counter > 0 else 'boom!'
+
 
 
 		pressed = pygame.key.get_pressed()	
@@ -354,6 +371,7 @@ def falling_game():
 
 
 		screen.blit(player_image,(playerX,playerY))
+		screen.blit(parachute_message,(pmX,pmY))
 
 #bird 1
 		screen.blit(bird,(birdX,birdY))
@@ -392,9 +410,12 @@ def falling_game():
 			buildingY=0
 
 		#TRIGGER FLAGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
-		if cityY <=297:
+		if counter >=20:#297 original
 			print("you done")
-			
+			pmX +=pmSpeed
+			pmSpeed += pmAccel
+			if pressed[pygame.K_p]:
+				return "safe"
 
 
 		if playerX<=186:
@@ -412,12 +433,45 @@ def falling_game():
 					if playerY+80 >= bird2Y:
 						return "dead"
 
+		displayText(screen,str(counter)+" seconds",500,0,60,255,0,0)
+
 			# player_image = explosion1
 
 
 		pygame.display.flip()
+		clock.tick(60)
 
 
+def chute_animation():
+	done = False
+	#fall variables
+	py = -539
+	p_image = player_chute
+	winx = -313
+	while not done:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				quit()
+
+		screen.blit(win1,(0,0))
+
+
+
+		screen.blit(p_image,(300,py))
+		screen.blit(win_text,(winx,300))
+
+
+		if py>= 261:
+			y=261
+			p_image = playerR
+			print("yay")
+			if winx <=400:
+				winx +=4
+			else:
+				winx=winx
+		else:
+			py+=4
+		pygame.display.flip()
 
 
 
@@ -433,6 +487,9 @@ if falling_game() == "dead":
 	pygame.mixer.music.play(-1,0.0)
 	fadetoWhite(screen,death1)
 	stillScene(death,0,0,pygame.K_SPACE)
+else:
+	print("poooooo")
+	chute_animation()
 
 # falling_game()
 
